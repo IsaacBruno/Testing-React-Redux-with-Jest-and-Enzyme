970f7e9701d39aaca48eb4ada29a8b7019789120
Isaac Bruno <isaacwinfor@gmail.com>
2021-08-11 20:25:27 -0300

    Posts reducer with tests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import successReducer from './successReducer';
+import postsReducer from './posts/reducer';
 
 export default combineReducers({
-  successReducer
+  postsReducer
 });
--- src/reducers/successReducer.js
+++
@@ -1,5 +0,0 @@
-const successReducer = (state, action) => {
-  return null;
-};
-
-export default successReducer;
